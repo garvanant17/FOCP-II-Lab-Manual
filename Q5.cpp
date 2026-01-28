3f609908db6d8873814fa23d50721b53d3436d57
garvanant17 <150727837+garvanant17@users.noreply.github.com>
2026-01-28 19:11:40 +0530

    Create Q5.cpp

---
+++ Q5.cpp
@@ -0,0 +1,13 @@
+//QUESTION 5
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter value of a and b: ";
+    cin>>a>>b;
+    c = a;
+    a = b;
+    b = c;
+    cout<<"after swapping, the values of a and b are "<<a<<","<<b;
+}
