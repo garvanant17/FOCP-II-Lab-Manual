6c33c2888db0547a7b69e791c456e338eeb54b88
garvanant17 <150727837+garvanant17@users.noreply.github.com>
2026-01-28 19:10:07 +0530

    Q2.cpp

---
+++ Q2.cpp
@@ -0,0 +1,11 @@
+//QUESTION 2
+
+#include <iostream>
+using namespace std;
+int main(){
+    float radius, area;
+    cout<<"enter the radius of the circle: ";
+    cin>>radius;
+    area = 3.14 * radius * radius;
+    cout<<"area= "<<area;
+}
