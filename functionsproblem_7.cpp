8920c05ff4a4d711bc034f119b2b6be62604e91e
garvanant17 <150727837+garvanant17@users.noreply.github.com>
2026-02-14 15:48:25 +0530

    Update functionsproblem_7.cpp

--- functionsproblem_7.cpp
+++ functionsproblem_7.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
 #include <cmath>
-#include <iomanip>
 using namespace std;
-double area();
-double area(){
-    double r,l,base,height,area,b;
-    int choice;
-    cout<<"1 for circle, 2 for rectangle, 3 for triangle: ";
-    cin>>choice;
-    if(choice == 1){
-        cout<<"radius: ";
-        cin>>r;
-        return 3.14 * pow(r,2);
-    }
-    else if (choice == 2){
-        cout<<"enter length and breadth: ";
-        cin>>l>>b;
-        return l*b;
-    }
-    else if(choice == 3){
-        cout<<"enter base and height: ";
-        cin>>base>>height;
-        return 0.5*base*height;
-
-    }
-    else return 0;
-
+double area(double r){
+    double ans;
+    return ans = 3.14*pow(r,2);
 }
-
+double area(double l, double b){
+double ans;
+return ans = l*b;
+}
+double area(double base,double height, int){
+    double ans;
+    return ans = 0.5*base*height;
+}
 int main(){
-    double result =  area();
-    cout<<"area: "<<result;
+    int choice;
+    cout<<"enter 1 for circle, 2 for rectangle , 3 for triangle: ";
+    cin>>choice;
+    if(choice==1){
+        double radius, result;
+        cout<<"radius: ";
+        cin>>radius;
+        result = area(radius);
+        cout<<"area: "<<result;
+    }
+    else if(choice==2){
+        double l, b, result;
+        cout<<"length and breadth: ";
+        cin>>l>>b;
+        result = area(l,b);
+        cout<<"area: "<<result;
+    }
+    else if(choice==3){
+        double base, height, result;
+        cout<<"base and height: ";
+        cin>>base>>height;
+        result = area(base, height, 1);
+        cout<<"area: "<<result;
+    }
+    else cout<<"invalid";
 }
