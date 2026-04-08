e1226a33c7647ca52d6c5257ab318685548b75fc
garvanant17 <150727837+garvanant17@users.noreply.github.com>
2026-04-08 15:35:48 +0530

    Implement shape classes with draw and area methods

---
+++ inheritanceQ7.cpp
@@ -0,0 +1,40 @@
+//Q7
+int main(){
+    Laptop l1;
+    cout<<"laptop:"<<endl;
+    l1.carry();
+    l1.powerOn();
+    l1.process();
+}
+
+#include <iostream>
+using namespace std;
+class Shape{
+    public:
+void draw(){
+    cout<<"draw"<<endl;
+}
+};
+class Circle:public Shape{
+    public:
+void area(){
+    cout<<"area of circle"<<endl;
+}
+};
+class Rectangle : public Shape{
+    public:
+    void area(){
+        cout<<"area of rectangle"<<endl;
+    }
+};
+
+int main(){
+    Circle c1;
+    Rectangle r1;
+    cout<<"circle: "<<endl;
+    c1.draw();
+    c1.area();
+    cout<<"rectangle: "<<endl;
+    r1.draw();
+    r1.area();
+}
